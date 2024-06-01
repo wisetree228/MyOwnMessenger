1c56d53ced5b70653266eccbc3a0834f2b1142b3
wisetree <pirumovgeorgij@gmail.com>
2024-06-01 14:52:42 +0300

    avatars of accounts are added!

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
 from flask import Flask, render_template, url_for, request, flash, get_flashed_messages, redirect, session, send_file, make_response, session
 import psycopg2
+import base64
+from werkzeug.utils import secure_filename
+from io import BytesIO
 
 def execute(query):
     try:
@@ -78,6 +81,25 @@ def checkId(query):
         print('Ошибка:', e)
 
 
+def setimg(query, file):
+    try:
+        con = psycopg2.connect(
+            database='base',
+            host='localhost',
+            user='postgres',
+            password='123456789',
+
+        )
+        cur=con.cursor()
+        cur.execute(query, (psycopg2.Binary(file),))
+        con.commit()
+        cur.close()
+        con.close()
+    except Exception as e:
+        print('Ошибка:', e)
+
+
+
 
 app=Flask(__name__)
 app.secret_key='key123456789'
@@ -86,13 +108,22 @@ app.secret_key='key123456789'
 def index():
 
     links=[]
+
     if 'userName' in session:
         chats = executeAll(f"SELECT * FROM chats WHERE user1='{session['userName']}' OR user2='{session['userName']}'")
         for i in chats:
             if i[0]==session['userName']:
-                links.append([i[1]])
+
+                if executeOne(f"SELECT img FROM users WHERE username='{i[1]}';")[0]:
+                    links.append([i[1], 1])
+                else:
+                    links.append([i[1], 0])
             else:
-                links.append([i[0]])
+
+                if executeOne(f"SELECT img FROM users WHERE username='{i[1]}';")[0]:
+                    links.append([i[0], 1])
+                else:
+                    links.append([i[0], 0])
 
 
 
@@ -105,11 +136,11 @@ def login():
         password=request.form['password']
         if len(username)>=5 and len(password)>=5 and not("'" in username or "'" in password):
             if not checkId(f"SELECT * FROM users WHERE username='{username}'"):
-                flash('такой учётной записи не существует!')
+                flash("That account doesn't exist!")
             else:
                 user=executeOne(f"SELECT * FROM users WHERE username='{username}'")
                 if user[2]!=password:
-                    flash('Неверный пароль!')
+                    flash('Incorrect password!!')
                 else:
                     session['userName']=username
                     return redirect(url_for('index'))
@@ -185,6 +216,69 @@ def chat(username):
 
 
 
+@app.route('/profile', methods=['POST', 'GET'])
+def profile():
+    if not 'userName' in session:
+        return redirect(url_for('login'))
+    if request.method == 'POST':
+        if 'image' in request.files:
+            image = request.files['image']
+            filename = secure_filename(image.filename)
+            image_data = image.read()
+            setimg(f"UPDATE users SET img=(%s) WHERE username='{session['userName']}';", image_data)
+        if 'password' in request.form:
+            password=request.form['password']
+            if password:
+                if not ("'" in password) and len(password)>=5:
+                    execute(f"UPDATE users SET password='{password}' WHERE username='{session['userName']}';")
+                    flash('password is changed!')
+                else:
+                    flash("The length of the password is at least 5 characters, it is forbidden to use ' in order to protect against sql injections!")
+
+
+    imgd=executeOne(f"SELECT img FROM users WHERE username='{session['userName']}';")[0]
+
+    if imgd:
+        #img_data_base64 = base64.b64encode(imgd).decode('utf-8')
+        return render_template('prof.html', session=session, img=imgd)
+    return render_template('prof.html', session=session, img=False)
+
+@app.route('/get-image/<name>')
+def get_image(name):
+    # Получение изображения из базы данных
+    image_data = executeOne(f"SELECT img FROM users WHERE username='{name}';")[0]
+
+    if image_data:
+        # Возвращение изображения в ответе Flask
+        return send_file(
+            BytesIO(image_data),
+            mimetype='image/png'
+        )
+    else:
+        image_data = executeOne(f"SELECT img FROM users WHERE username='empty';")[0]
+        return send_file(
+            BytesIO(image_data),
+            mimetype='image/png'
+        )
+
+
+
+@app.route('/delprof')
+def delprof():
+    if not 'userName' in session:
+        return redirect(url_for('login'))
+    s=session['userName']
+    session.pop('userName', None)
+    execute(f"DELETE FROM users WHERE username='{s}';")
+    execute(f"DELETE FROM chats WHERE user1='{s}' OR user2='{s}';")
+    execute(f"DELETE FROM mess WHERE auth='{s}' OR get='{s}';")
+    return redirect(url_for('index'))
+
+
+
+
+
+
 @app.errorhandler(404)  # обработчик неверного URL
 def pagenotfound(error):
     return render_template('error.html')
@@ -195,5 +289,6 @@ def pagenotfound(error):
 
 
 
+#
 if __name__=="__main__":
     app.run(debug=True)
